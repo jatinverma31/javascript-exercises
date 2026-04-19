2d76a122f6232a750a72cee29ed51ab58a3e7819
jatinverma31 <jatinvermax821@gmail.com>
2026-04-19 22:31:42 +0530

    Solve js foundation exercises from 6 to 10

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+
+    if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+
+    sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
